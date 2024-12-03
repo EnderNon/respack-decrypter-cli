69288185c1d5afbfd04d4e9a88acf23edd5b98f4
endernon <ender_non@outlook.com>
2024-12-03 23:10:22 +0000

    current progress

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,91 @@
 use image::ImageBuffer;
 use zune_png::{zune_core::options::DecoderOptions, PngDecoder};
+use clap::Parser;
+use glob::glob;
+use std::fs::metadata;
+use std::path::{Path, PathBuf};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Input path
+    #[arg(short, long)]
+    path: Vec<PathBuf>,
+
+    /// Output path
+    #[arg(short, long)]
+    out: Option<PathBuf>,
+
+    /// Debug mode
+    #[arg(short, long, default_value_t=false)]
+    debug: bool,
+
+}
 
 fn main() {
+
+    // argument parsing yep
+    let Args: Args = Args::parse();
+    let pathfr = Args.path.clone();
+    let outfr = match Args.out {
+        Some(realoutpath) => {
+            realoutpath
+        },
+        None => {
+            Args.path
+        }
+    };
+    let realdebugmode = Args.debug;
+
+    let mut filelist: Vec<String> = Vec::new();
+
+
+    let pathtype = metadata(&pathfr).unwrap();
+    if pathtype.is_file() {
+
+    }
+//    else if pathtype.is_dir() {
+//        // parse file list if it's a dir
+//        for entry in glob(&format!("{}/**/*.png", pathfr.clone().display())).expect("Failed to read glob pattern (you should panic)") {
+//            match entry {
+//                Ok(path) => {
+//                    // println!("{:?}", path.display());
+//                    filelist.push(path.display().to_string());
+//                },
+//                Err(e) => {
+//                    println!("Shell globbing error");
+//                }
+//            }
+//        }
+//    }
+//    else {
+//        panic!("")
+//    }
+//
+
+
+    if realdebugmode {
+        for i in filelist {
+            println!("{i}")
+        }
+    }
+
+
     let data = include_bytes!("../inventory.png");
+
+
+
 
     let mut decoder = PngDecoder::new_with_options(data, DecoderOptions::new_cmd());
 
     decoder.decode_headers().unwrap();
     let dim = decoder.get_dimensions().unwrap();
+    //dbg!(dim);
     let col = decoder.get_colorspace().unwrap();
+    //dbg!(col);
     let data = decoder.decode_raw().unwrap();
+    //dbg!(&data);
+    let depth = decoder.get_depth().unwrap();
 
     // TODO: figure out propper color checks rn we assume data is RGBA
 
